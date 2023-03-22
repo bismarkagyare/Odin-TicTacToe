aff7224905b8c8faa4be97197b1f622cd2e5d756
Bismark Agyare <bismarkagyare54@gmail.com>
2023-03-22 11:09:53 +0000

    created the gameboard grid as a modal

--- index.js
+++ index.js
@@ -4,12 +4,34 @@ const Gameboard = (() => {
   const render = () => {
     let boardHTML = '';
     gameboard.forEach((box, index) => {
-      boardHTML += `<div class="box" id="box-${index}">${box}</div>`;
+      boardHTML += `<div class="box grid-cell" id="${index}">${box}</div>`;
     });
-    document.getElementById('gameboard').innerHTML = boardHTML;
+    document.querySelector('.gameboard').innerHTML = boardHTML;
+    document.querySelector('.container').style.display = 'none';
+    document.querySelector('.gameboard').classList.remove('hidden');
   }
 
-  return {render};
+  const createGrid = () => {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box, i) => {
+      box.classList.add('grid-cell');
+      if (i < 3) {
+        box.classList.add('border-top');
+      }
+      if (i % 3 === 0) {
+        box.classList.add('border-left');
+      }
+      if (i % 3 === 2) {
+        box.classList.add('border-right');
+      }
+      if (i > 5) {
+        box.classList.add('border-bottom');
+      }
+    });
+  };
+  
+
+  return {render, createGrid};
 })();
 
 const createPlayer = (name, sign) => {
@@ -18,5 +40,6 @@ const createPlayer = (name, sign) => {
 
 const startBtn = document.getElementById('start-button');
 startBtn.addEventListener('click', () => {
-  alert('hi');
+  Gameboard.render();
+  Gameboard.createGrid();
 });
